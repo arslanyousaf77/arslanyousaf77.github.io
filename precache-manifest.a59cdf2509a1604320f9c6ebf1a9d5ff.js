4fae844683299e7dbbf2e3c9891cdb2188e61e10
arslanyousaf77 <arslanyousaf77@gmail.com>
2020-07-09 15:22:30 +0500

    Updates

---
+++ precache-manifest.a59cdf2509a1604320f9c6ebf1a9d5ff.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a741318a24b89fb6488d715637f69683",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9239b24af115a416a03",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "60f1411f13c6af0b0fe8",
+    "url": "/static/css/main.8dd07fa0.chunk.css"
+  },
+  {
+    "revision": "f9239b24af115a416a03",
+    "url": "/static/js/2.a957d927.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.a957d927.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60f1411f13c6af0b0fe8",
+    "url": "/static/js/main.cf54f578.chunk.js"
+  },
+  {
+    "revision": "397168f6caa8c63849a9",
+    "url": "/static/js/runtime-main.7ccc1417.js"
+  },
+  {
+    "revision": "9c9c187e11e0592f080554b6d6594c86",
+    "url": "/static/media/arslan-logo.9c9c187e.png"
+  },
+  {
+    "revision": "5378dc9733c5e4b4a77d7f07799289c5",
+    "url": "/static/media/arslan.5378dc97.jpg"
+  }
+]);
